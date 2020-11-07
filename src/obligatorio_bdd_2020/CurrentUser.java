ad3d61781f7441b2f48cef5016ff16e68e6b44e5
AleEtchevers <32549007+AleEtchevers@users.noreply.github.com>
2020-11-07 11:06:16 -0300

    current user

---
+++ src/obligatorio_bdd_2020/CurrentUser.java
@@ -0,0 +1,47 @@
+public class CurrentUser {
+
+    private int id;
+
+    private String rol;
+
+    private String alias;
+
+    private static CurrentUser usuario;
+
+    public CurrentUser(){
+    }
+
+    public static CurrentUser getCurrentUser(){
+        if (usuario == null){
+            usuario = new CurrentUser();
+        }
+        return usuario;
+    }
+
+    public int get_ID(){
+        return this.id;
+    }
+
+    public void setUser_id(int id){
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getName_Role(){
+        return new StringBuilder().append(this.alias).append(this.rol).toString();
+    }
